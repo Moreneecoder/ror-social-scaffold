71af74efbd89725f50d1b9bb361ca8ea07e07054
Morenikeji Bello <caeserselassie@gmail.com>
2021-04-30 21:52:47 +0100

    Add friendship model and migration

--- db/migrate/20210430203600_create_friendships.rb
+++ db/migrate/20210430203600_create_friendships.rb
@@ -2,12 +2,11 @@ class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
       t.references :user, foreign_key: true
-      t.references :friend, foreign_key: true
+      t.references :friend #foreign_key: true
       t.boolean :status
 
       t.timestamps
     end
-    
     add_foreign_key :friendships, :users, column: :friend_id
   end
 end
