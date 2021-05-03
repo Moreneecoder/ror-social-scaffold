43a61c36864dd31079207a2e59cc02622d3de075
Morenikeji Bello <caeserselassie@gmail.com>
2021-05-03 22:38:55 +0100

    Add send friend request logic via routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  post 'users/:id/request', to: 'users#request_friend'
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
