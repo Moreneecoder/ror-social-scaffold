308d466084edbaf771822ed89528b6c637d85e8e
Morenikeji Bello <caeserselassie@gmail.com>
2021-05-05 22:31:22 +0100

    Write capybara test for decline friend request logic

--- spec/users_controller_spec.rb
+++ spec/users_controller_spec.rb
@@ -16,15 +16,14 @@ RSpec.describe 'Friendship', type: :feature do
         expect(current_path).to eq("/users")
     end
 
-    # scenario 'decline friend' do
-    #     current_user = User.create!(name: 'Tester', email: 'test@example.com', password: 'f4k3p455w0rd')
-    #     login_as(current_user, scope: :user)
-    #     user.friendships.build(friend_id: current_user.id, status: false).save
+    scenario 'decline friend' do
+        current_user = User.create!(name: 'Tester', email: 'test@example.com', password: 'f4k3p455w0rd')
+        login_as(current_user, scope: :user)
+        user.friendships.build(friend_id: current_user.id, status: false).save
         
-    #     visit users_url
-    #     visit "/users/#{user.id}/decline"
-    #     sleep(3)
-    #     expect(current_path).to have_content('Friend Invitation has been sent.')
-    #     #expect(page).to have_content('Friend Invitation has been sent.')
-    # end
+        visit users_url
+        visit "/users/#{user.id}/decline"
+        sleep(3)
+        expect(page).to have_content("You Declined #{user.name}'s Friend Request!")
+    end
 end
