bd4bd942ed0bc0cd26d9107ac7926d2e54e1d8bb
Morenikeji Bello <caeserselassie@gmail.com>
2021-05-05 16:53:43 +0100

    Write test for friendship belongs to user association

---
+++ spec/frienship_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'associations' do
+    it 'belongs to a user' do
+      friendship = Friendship.reflect_on_association(:user)
+      expect(friendship.macro).to eq(:belongs_to)
+    end
+
+  end
+end
