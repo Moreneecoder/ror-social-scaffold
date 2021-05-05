52d282ae4b311484c9fe217690e1c3409b537f0c
Morenikeji Bello <caeserselassie@gmail.com>
2021-05-05 17:24:29 +0100

    Rename user_controller.spec to authentication_spec

--- spec/authentication_spec.rb
+++ spec/authentication_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe 'Authenticate', type: :feature do
       sleep(3)
       expect(current_path).to eq("/")
     end
+
+    scenario 'register' do
+        visit new_user_registration_url
+        fill_in 'user_email', with: user.email
+        fill_in 'user_password', with: user.password
+        click_button 'Log in'
+        sleep(3)
+        expect(current_path).to eq("/")
+    end
   end
