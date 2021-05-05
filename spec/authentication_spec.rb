7763883f26987fa71f9b9a546789362659b392dc
Morenikeji Bello <caeserselassie@gmail.com>
2021-05-05 17:34:20 +0100

    Write capybara test for user sign up process

--- spec/authentication_spec.rb
+++ spec/authentication_spec.rb
@@ -13,11 +13,14 @@ RSpec.describe 'Authenticate', type: :feature do
     end
 
     scenario 'register' do
-        visit new_user_registration_url
-        fill_in 'user_email', with: user.email
-        fill_in 'user_password', with: user.password
-        click_button 'Log in'
-        sleep(3)
-        expect(current_path).to eq("/")
+      visit new_user_registration_url
+      fill_in 'user_name', with: 'JaneDoe'
+      fill_in 'user_email', with: 'janedoe@example.com'
+      fill_in 'user_password', with: '123456'
+      fill_in 'user_password_confirmation', with: '123456'
+      click_button 'Sign up'
+      sleep(3)
+
+      expect(current_path).to eq("/")
     end
-  end
+end
